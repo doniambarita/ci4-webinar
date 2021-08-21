e10d11791f461ee93be1859fbc494df6d60355af
doniambarita <doniambarita123@gmail.com>
2021-08-21 10:39:39 +0700

    update Pages controller

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,25 +1,49 @@
 <?php
 
 namespace App\Controllers;
+use \App\Models\WebinarModel;
+use \App\Models\WebinarPesertaModel;
+
 
 class Pages extends BaseController
 {
+
+    protected $webinarModel;
+    protected $webinarPesertaModel;
+
+    public function __construct()
+    {
+        $this->webinarModel = new WebinarModel();
+        $this->webinarPesertaModel = new WebinarPesertaModel();
+    }
+    
 	public function index()
 	{   
         $data = [
-            'title' => 'Seputar Webinar',
-            'slug' => '5-library-android-yang-patut-kamu-coba-di-2021'
+            'title' => 'Seputar Webinar'
         ];	
+        
 		return view('pages/landingpage', $data);
 	}
 
-    public function detail($slug)
+    public function detail($slug = false)
     {
         $data = [
             'title' => 'Detail Webinar',
-            'judul' => '5 Library Android yang Patut Kamu Coba di 2021',
-            'slug' => $slug,
+            'webinar' => $this->webinarModel->getWebinar($slug)
         ];
+
+        if(empty($data['webinar']) || empty($slug)) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Judul Webinar ' . $slug . ' tidak ditemukan');
+        }
+
+        $data['is_register'] = $this->webinarPesertaModel->getWebinarPeserta($data['webinar']['webinar_id'], user_id());
+
+        if(empty($data['is_register'])){
+            $data['is_register'] = 0;
+        }else{
+            $data['is_register'] = 1;
+        }
         return view('pages/detail_webinar', $data);
     }
 
